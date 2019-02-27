30a9e5f86729f5a69f09724f7d77fe4a3891a28d
dosovits <dosovits@users.noreply.github.com>
2019-02-27 13:10:30 +0100

    Update process_images.py

--- process_images.py
+++ process_images.py
@@ -1,8 +1,3 @@
-# Copyrights. All rights reserved.
-# ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland,
-# Space Center (eSpace), 2018
-# See the LICENSE.TXT file for more details.
-
 import torch
 
 from practical_deep_stereo import pds_network
